7233485edb981560d038f2ea35d4305c3aca52a3
Skipin Maksim <skipysmaks@gmail.com>
2021-07-18 14:43:12 +0300

    deleted search operation file

--- src/redux/searchData/index.js
+++ src/redux/searchData/index.js
@@ -1,4 +1,3 @@
 export { default as searchDataActions } from "./searchDataActions";
 export { default as searchDataReducers } from "./searchDataReducers";
 export { default as searchDataSelectors } from "./searchDataSelectors";
-export { default as searchDataOperations } from "./searchDataOperations";
