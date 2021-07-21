00945d1ef9c5b04ca9f571e524a49867e806d2ef
Skipin Maksim <skipysmaks@gmail.com>
2021-07-21 16:02:48 +0300

    added debounce

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory, useLocation } from "react-router-dom";
 import queryString from "query-string";
+import debounce from "lodash.debounce";
 
 import { Pagination } from "antd";
 
@@ -16,7 +17,9 @@ import RepositoriesList from "./components/RepositoriesList/RepositoriesList";
 const App = () => {
     const dispatch = useDispatch();
     const history = useHistory();
-    const location = useLocation();
+    const { search } = useLocation();
+
+    const [inputSearchValue, setInputSearchValue] = useState("");
 
     const { getSearchValue, getPerPage, getPage } = searchDataSelectors;
     const { getAllRepositoriesList, getTotalCount } = gitDataSelectors;
@@ -29,18 +32,27 @@ const App = () => {
 
     if (totalCount > 1000) totalCount = 1000;
 
-    const qs = {
-        q: currentSearchValue,
-        per_page: currentSearchPerPage,
-        page: currentSearchPage,
+    const getQueryString = () => {
+        const qs = queryString.parse(search);
+
+        qs.q = currentSearchValue;
+        qs.per_page = currentSearchPerPage;
+        qs.page = currentSearchPage;
+
+        return qs;
     };
 
     const fetchSearch = () => {
-        const { q, per_page, page } = queryString.parse(location.search);
-        dispatch(gitDataOperations.fetchRepositories(q, per_page, page));
+        const { q, per_page, page } = queryString.parse(search);
+
+        if (q) {
+            dispatch(gitDataOperations.fetchRepositories(q, per_page, page));
+            setInputSearchValue(q);
+        }
     };
 
     const updatePage = page => {
+        const qs = getQueryString();
         qs.page = page;
         history.push(`?${queryString.stringify(qs)}`);
     };
@@ -57,19 +69,50 @@ const App = () => {
         updatePage(e);
     };
 
+    const handleOnChangeInput = e => {
+        setInputSearchValue(e.target.value);
+        delayedQuery(e.target.value);
+    };
+
+    const handleSubmit = (value, e) => {
+        if (e) e.preventDefault();
+
+        if (value) {
+            const qs = getQueryString();
+            qs.q = value;
+
+            history.push(`?${queryString.stringify(qs)}`);
+        }
+    };
+
+    const delayedQuery = useCallback(
+        debounce(value => handleSubmit(value), 1500),
+        [debounce],
+    );
+
     useEffect(() => {
-        if (!location.search) {
+        const qs = getQueryString();
+
+        if (!search) {
             qs.q = "react";
             history.push(`?${queryString.stringify(qs)}`);
+            setInputSearchValue(qs.q);
+
             return;
         }
 
         fetchSearch();
-    }, [location.search]);
+    }, [search]);
 
     return (
         <div className={s.container}>
-            <form>Тут будет инпут</form>
+            <form onSubmit={e => handleSubmit(inputSearchValue, e)}>
+                <input
+                    type="text"
+                    value={inputSearchValue}
+                    onChange={handleOnChangeInput}
+                />
+            </form>
 
             <div className={s.content}>
                 {!repositoriesList.length && (
